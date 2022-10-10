bdb1899fe39afc79b625b569719f02e45bfe904b
Oscar-devjava <negociante2t@gmail.com>
2022-10-10 18:52:30 -0300

    busca veiculo por id

--- src/main/java/br/com/tinnova/veiculo/veiculo/infra/VeiculoInfraRepository.java
+++ src/main/java/br/com/tinnova/veiculo/veiculo/infra/VeiculoInfraRepository.java
@@ -1,11 +1,14 @@
 package br.com.tinnova.veiculo.veiculo.infra;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import br.com.tinnova.veiculo.veiculo.application.repository.VeiculoRepository;
 import br.com.tinnova.veiculo.veiculo.domain.Veiculo;
+import br.com.tinnova.veiculo.veiculo.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @Repository
@@ -29,6 +32,15 @@ public class VeiculoInfraRepository implements VeiculoRepository {
 		var buscaVeiculos = veiculoSpringDataJPARepository.findAll();
 		log.info("[finaliza] VeiculoInfraRepository - buscaTodosVeiculos ");
 		return buscaVeiculos;
+	}
+
+	@Override
+	public Veiculo buscaVeiculoPorId(UUID idVeiculo) {
+		log.info("[inicia] VeiculoInfraRepository - buscaVeiculoPorId ");
+		var buscaVeiculo = veiculoSpringDataJPARepository.findById(idVeiculo).orElseThrow(() -> APIException.build(HttpStatus.NOT_EXTENDED,
+				"Veículo não encontrado, certifique-se que o id informado está correto"));
+		log.info("[finaliza] VeiculoInfraRepository - buscaVeiculoPorId ");
+		return buscaVeiculo;
 	}
 
 }
