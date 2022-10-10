149f1c8c2def8f6658693193991ce3f7c97cfbb9
Oscar-devjava <negociante2t@gmail.com>
2022-10-10 18:11:01 -0300

    implementa camada de api

--- src/main/java/br/com/tinnova/veiculo/veiculo/application/api/VeiculoAPI.java
+++ src/main/java/br/com/tinnova/veiculo/veiculo/application/api/VeiculoAPI.java
@@ -1,8 +1,11 @@
 package br.com.tinnova.veiculo.veiculo.application.api;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +17,8 @@ public interface VeiculoAPI {
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
 	VeiculoResponse postCadastraNovoVeiculo(@Valid @RequestBody VeiculoRequest veiculoRequest );
+	
+	@GetMapping
+	@ResponseStatus(value = HttpStatus.OK)
+	List<VeiculosListResponse> getVisualizaTodosVeiculos();
 }
