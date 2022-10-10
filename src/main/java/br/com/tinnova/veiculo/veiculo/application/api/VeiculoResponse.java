4bfcc33fc223b012502fbd4b771b9bf8e1b5e6dd
Oscar-devjava <negociante2t@gmail.com>
2022-10-10 17:38:04 -0300

    implementa classe de response

---
+++ src/main/java/br/com/tinnova/veiculo/veiculo/application/api/VeiculoResponse.java
@@ -0,0 +1,12 @@
+package br.com.tinnova.veiculo.veiculo.application.api;
+
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Builder
+@Value
+public class VeiculoResponse {
+	private UUID idAutomovel;
+}
