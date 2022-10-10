8712486a598a66c9e21b8d6c42ddd6437f5eaaf8
Oscar-devjava <negociante2t@gmail.com>
2022-10-10 18:04:23 -0300

    implementa camada de infra e salva

--- src/main/java/br/com/tinnova/veiculo/veiculo/application/repository/VeiculoRepository.java
+++ src/main/java/br/com/tinnova/veiculo/veiculo/application/repository/VeiculoRepository.java
@@ -1,5 +1,9 @@
 package br.com.tinnova.veiculo.veiculo.application.repository;
 
+import br.com.tinnova.veiculo.veiculo.domain.Veiculo;
+
 public interface VeiculoRepository {
+
+	Veiculo salva(Veiculo veiculo);
 
 }
